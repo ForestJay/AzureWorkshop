a85be3132d1d9e9cc4e7ba058e29fc7ab876650e
Forest Handford <foresthandford@gmail.com>
2014-11-10 19:35:08 +0000

    Add solution from Azure Mobile Services excluding NuGet packages

---
+++ AMSLabFJHService/DataObjects/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace AMSLabFJHService.DataObjects
+{
+    public class TodoItem : EntityData
+    {
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
